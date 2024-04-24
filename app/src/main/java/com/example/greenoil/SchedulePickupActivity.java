ea1d5585d23771d188759672e68698a9d2a2628e
FZX21 <hyperiskz@gmail.com>
2024-04-24 08:42:12 +0300

    SchedulePickup Functionality

--- app/src/main/java/com/example/greenoil/SchedulePickupActivity.java
+++ app/src/main/java/com/example/greenoil/SchedulePickupActivity.java
@@ -8,7 +8,9 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.app.TimePickerDialog;
 import android.widget.TimePicker;
@@ -25,11 +27,21 @@ public class SchedulePickupActivity extends AppCompatActivity {
     private TextView mDisplayTime;
     private TimePickerDialog.OnTimeSetListener mTimeSetListener;
     private TextView changeAddressText;
+    private ImageButton backToOptionsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_pickup);
+
+        backToOptionsBtn = findViewById(R.id.backToOptionsBtn);
+
+        backToOptionsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backToScheduleOptions();
+            }
+        });
 
         valueTextView = findViewById(R.id.quantityAmount);
         updateValueText();
@@ -43,8 +55,9 @@ public class SchedulePickupActivity extends AppCompatActivity {
             }
         });
 
+
         mDisplayDate = (TextView) findViewById(R.id.tvDate);
-        mDisplayTime = findViewById(R.id.tvTime);
+        mDisplayTime = (TextView) findViewById(R.id.tvTime);
 
         //DatePicker
         mDisplayDate.setOnClickListener(new View.OnClickListener() {
@@ -56,26 +69,32 @@ public class SchedulePickupActivity extends AppCompatActivity {
                 int month = cal.get(Calendar.MONTH);
                 int day = cal.get(Calendar.DAY_OF_MONTH);
 
-                // Set the maximum date by a week & minimum date by today
-                cal.add(Calendar.WEEK_OF_YEAR, 1);
-                int maxYear = cal.get(Calendar.YEAR);
-                int maxMonth = cal.get(Calendar.MONTH);
-                int maxDay = cal.get(Calendar.DAY_OF_MONTH);
+//                // Set the maximum date by a week & minimum date by today
+//                cal.add(Calendar.WEEK_OF_YEAR, 1);
+//                int maxYear = cal.get(Calendar.YEAR);
+//                int maxMonth = cal.get(Calendar.MONTH);
+//                int maxDay = cal.get(Calendar.DAY_OF_MONTH);
 
                 cal = Calendar.getInstance();
                 int minYear = cal.get(Calendar.YEAR);
                 int minMonth = cal.get(Calendar.MONTH);
                 int minDay = cal.get(Calendar.DAY_OF_MONTH);
 
+
                 DatePickerDialog dialog = new DatePickerDialog(SchedulePickupActivity.this,
-                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        android.R.style.Theme_DeviceDefault_Light_Dialog_MinWidth,
                         mDateSetListener,
                         year,month,day);
-                // Set the maximum & minimum date
-                dialog.getDatePicker().setMaxDate(cal.getTimeInMillis());
                 dialog.getDatePicker().setMinDate(cal.getTimeInMillis());
 
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                cal.add(Calendar.WEEK_OF_YEAR, 2);
+                int maxYear = cal.get(Calendar.YEAR);
+                int maxMonth = cal.get(Calendar.MONTH);
+                int maxDay = cal.get(Calendar.DAY_OF_MONTH);
+
+                // Set the maximum & minimum date
+                dialog.getDatePicker().setMaxDate(cal.getTimeInMillis());
+
                 dialog.show();
             }
         });
@@ -94,12 +113,13 @@ public class SchedulePickupActivity extends AppCompatActivity {
         mDisplayTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Calendar cal = Calendar.getInstance();
                 int hour = cal.get(Calendar.HOUR_OF_DAY);
                 int minute = cal.get(Calendar.MINUTE);
 
                 // Calculate minimum time (1 hour from current time)
-                cal.add(Calendar.HOUR_OF_DAY, 1);
+                cal.add(Calendar.HOUR_OF_DAY, 3);
                 int minHour = cal.get(Calendar.HOUR_OF_DAY);
                 int minMinute = cal.get(Calendar.MINUTE);
 
@@ -110,26 +130,40 @@ public class SchedulePickupActivity extends AppCompatActivity {
                 int maxMinute = cal.get(Calendar.MINUTE);
 
                 TimePickerDialog dialog = new TimePickerDialog(SchedulePickupActivity.this,
-                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        android.R.style.Theme_DeviceDefault_Dialog_MinWidth,
                         mTimeSetListener,
                         hour, minute, false);
 
-                // Set minimum time
-                dialog.getButton(TimePickerDialog.BUTTON_POSITIVE).setTextColor(Color.BLUE);
-                dialog.getButton(TimePickerDialog.BUTTON_NEGATIVE).setTextColor(Color.RED);
 
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
                 dialog.show();
             }
         });
         mTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            boolean flag = false;
 
             public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
                 Log.d(TAG, "onTimeSet: hh:mm: " + hourOfDay + ":" + minute);
-
+                minute = roundToNearest30Minutes(minute);
+                if (flag == true){
+                    hourOfDay++;
+                }
                 String time = String.format(Locale.getDefault(), "%02d:%02d", hourOfDay, minute);
+                flag=false;
                 mDisplayTime.setText(time);
             }
+            private int roundToNearest30Minutes(int minute) {
+                if (minute >= 45) {
+                    flag = true;
+                    return 0;
+                } else if (minute >= 30) {
+                    return 30;
+                } else if (minute >= 15) {
+                    return 30;
+                } else {
+                    return 0;
+                }
+            }
+
         };
     }
 
@@ -142,17 +176,24 @@ public class SchedulePickupActivity extends AppCompatActivity {
 
     public void subtractValue(View view) {
         if (value > 5) {
-            value--;
+            value-=0.5;
             updateValueText();
         }
     }
 
     private void updateValueText() {
+        if (value < 5){
+            value = 5;
+        }
         valueTextView.setText(String.valueOf(value));
     }
 
     private void changeAddressPicker() {
         Intent intent = new Intent(this, LocationPickerActivity.class);
+        startActivity(intent);
+    }
+    private void backToScheduleOptions() {
+        Intent intent = new Intent(this, ScheduleOptions.class);
         startActivity(intent);
     }
 
