3e2edcb77de5c13eb08d51cf8eba17775220cd3b
Abdulaziz alhumidi <abdulazizalhumidi06@gmail.com>
2024-05-10 02:31:42 +0300

    Customer service
    Customer service has been completed

--- app/src/main/java/com/example/greenoil/Service.java
+++ app/src/main/java/com/example/greenoil/Service.java
@@ -3,26 +3,63 @@ package com.example.greenoil;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class Service extends AppCompatActivity {
 
+    ImageButton backBtn, sendBut;
+    RecyclerView recyclerView;
+    EditText messageEditText;
+    List<Message> messageList;
+    MessageAdapter messageAdapter;
+    public static final MediaType JSON = MediaType.get("application/json");
+    OkHttpClient client = new OkHttpClient();
 
-    ImageButton backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
 
-
         backBtn = findViewById(R.id.backBut);
+        recyclerView = findViewById(R.id.recyclerview);
+        messageEditText = findViewById(R.id.messageText);
+        sendBut = findViewById(R.id.sendBut);
+
+        messageList = new ArrayList<>();
+
+        messageAdapter = new MessageAdapter(messageList);
+        recyclerView.setAdapter(messageAdapter);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setStackFromEnd(true);
+        recyclerView.setLayoutManager(llm);
 
 
         backBtn.setOnClickListener(new View.OnClickListener() {
@@ -34,5 +71,27 @@ public class Service extends AppCompatActivity {
             }
         });
 
+        sendBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String question = String.valueOf(messageEditText.getText()).trim();
+                addToChat(question,Message.SENT_BY_ME);
+//                callAPI(question);
+                messageEditText.setText("");
+            }
+        });
     }
+
+    void addToChat(String message,String sentBy){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messageList.add(new Message(message,sentBy));
+                messageAdapter.notifyDataSetChanged();
+                recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
+            }
+        });
+    }
+
 }
